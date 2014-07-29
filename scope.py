8240eb2cef8951514a9e5a209f1e531a0170df14
Asher <asherehsa@gmail.com>
2014-07-29 13:57:26 -0700

    scope spam

--- scope.py
+++ scope.py
@@ -7,4 +7,20 @@ def eggs():
     print('In egg():',spam)
 def ham():
     print('In ham():', spam) #spam in this function is global
-    
+
+def bacon():
+    global spam #le global spam
+    print("In bacon():", spam)
+    spam = 0
+#def CRASH():
+#    print(spam) #local in this function
+#    spam = 0
+
+print(spam)
+eggs()
+print(spam)
+ham()
+print(spam)
+bacon()
+print(spam)
+#CRASH()
